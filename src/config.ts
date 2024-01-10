f529bbdce1dc13a8ab4821b58c51801183c45afa
Kapil Rathore <116137491+rathorekapil20@users.noreply.github.com>
2024-01-10 12:43:58 +0530

    cv

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Portfolio | Kapil Rathore';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = "Welcome to my portfolio! I'm a Web Developer and Graphic Designer passionate about crafting seamless digital experiences. Explore my projects showcasing expertise in front-end development, UI/UX design, and custom graphics. Using Astro with Tailwind CSS, I create fast, modern websites. Let's collaborate on your next digital venture!";
